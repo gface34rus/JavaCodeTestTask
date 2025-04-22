ec48199c72a2d62777c64c295328f4fb56df9505
gface <89259112970@mail.ru>
2025-04-22 14:49:13 +0300

    Initial Commit

---
+++ src/main/java/com/javacode/JCTestTask/JavaCodeTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.javacode.JCTestTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodeTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCodeTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javacode/JCTestTask/JavaCodeTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacode.JCTestTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodeTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
