9352bb3075666f06121744b9f87d3adb768a38cb
gface <89259112970@mail.ru>
2025-04-23 11:37:37 +0300

    add more fix

--- src/main/java/com/javacode/JCTestTask/dto/WalletRequest.java
+++ src/main/java/com/javacode/JCTestTask/dto/WalletRequest.java
@@ -1,13 +1,17 @@
 package com.javacode.JCTestTask.dto;
 
 import com.javacode.JCTestTask.model.OperationType;
+import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
 public class WalletRequest {
+    @NotNull
     private UUID walletId;
+    @NotNull
     private OperationType operationType;
+    @NotNull
     private BigDecimal amount;
 
     public UUID getWalletId() {
--- src/main/java/com/javacode/JCTestTask/exception/GlobalExceptionHandler.java
+++ src/main/java/com/javacode/JCTestTask/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -16,5 +17,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("Internal server error"));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException e) {
+        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
     }
 }
